be0e49a3bb4104cc9e62d9f5f1c274dca421ce8d
Lado Lomidze <lado.lomidze@gmail.com>
2017-08-25 19:48:39 +0400

    🎨 Style: Update bulma and fix breaking changes.

---
+++ src/data/frameworks/BlazeCSS.js
@@ -0,0 +1,18 @@
+export const BlazeCSS = {
+  isNew: true,
+  name: 'Blaze CSS',
+  slug: 'blaze',
+  install: 'blaze',
+  description:
+    'Open source modular CSS toolkit providing great structure for building websites quickly',
+  url: 'http://blazecss.com/',
+  documentation: 'http://blazecss.com/getting-started/install/',
+  github: 'https://github.com/BlazeCSS/blaze',
+  version: '3.3.0',
+  css: 'SASS',
+  js: false,
+  stats: {
+    stars: '1.1K+',
+    forks: '100+'
+  }
+};
